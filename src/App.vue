0f80e4388ca567cea8d9eabab0c56827787cde03
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-06-02 12:47:45 +0200

    feat(editor): install and setup editorjs + headings blocks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+import {
+  default as EditorJS,
+  type ToolConstructable,
+} from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+
+new EditorJS({
+  holder: "editor",
+  tools: {
+    header: {
+      class: Header as unknown as ToolConstructable,
+      shortcut: "CMD+SHIFT+H",
+      config: {
+        placeholder: "Enter a header",
+        levels: [1, 2, 3, 4, 5, 6],
+      },
+    },
+  },
+});
+</script>
+
 <template>
   <div class="container">
     <h1>Welcome to Tauri!</h1>
+    <div id="editor"></div>
   </div>
 </template>
+
+<style scoped>
+#editor {
+  border: 1px solid #000;
+}
+</style>
